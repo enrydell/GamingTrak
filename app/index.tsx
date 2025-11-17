885f18540cb22ea07f4c555f3cba96a4a4317dec
Henrique Dell <henriquedell1@gmail.com>
2025-11-17 12:22:42 -0300

    chore: Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'expo-router';
+import React from 'react';
+import { StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10
+  },
+});
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href="/search">Search</Link>
+    </View>
+  );
+}
